22a37b98e065703363326e725d562d0bc77db73f
harshpa324 <harshpa324@gmail.com>
2025-05-18 16:10:01 +0530

    error

--- outputs/code/54658afe-6932-424b-9d47-3aa5d8158e63.py
+++
@@ -1,8 +0,0 @@
-
-# Dummy code - not used
-from manim import *
-
-class APIErrorDemo(Scene):
-    def construct(self):
-        # This code won't be executed as we're using a direct URL
-        pass
